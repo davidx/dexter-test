9259d581c64f8509417e4bac458d53f6fbb905f8
David Andersen <davidx@gmail.com>
2025-04-06 23:57:02 -0400

    Fix: ADD_NEGATIVE_TEST_CASE, PARAMETERIZE_TEST_DATA, REFACTOR_TEST_CASE, ADD_CONTENT_TYPE_ASSERTION (4 issues)

--- test/test.py
+++ test/test.py
@@ -5,5 +5,14 @@ class TestDataEndpoint(unittest.TestCase):
     def test_data_endpoint(self):
         response = self.app.get('/data')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), [1, 2, 3, 4, 5])
 
+        data = response.get_json()
+        self.assertEqual(data, [1, 2, 3, 4, 5])
+
+
+def test_invalid_endpoint(self):
+    response = self.app.get('/invalid')
+    self.assertEqual(response.status_code, 404)
+
+
+self.assertEqual(response.headers['Content-Type'], 'application/json')
