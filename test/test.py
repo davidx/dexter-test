7a4e7eaa6517b2a82ac1f50786e69b2cb5f0e08f
David Andersen <davidx@Davids-Mac-mini.local>
2025-04-06 23:52:37 -0400

    add

---
+++ test/test.py
@@ -0,0 +1,9 @@
+class TestDataEndpoint(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_data_endpoint(self):
+        response = self.app.get('/data')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), [1, 2, 3, 4, 5])
+
