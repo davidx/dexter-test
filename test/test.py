c2ba625488967df1f3d4892538aca0cb07820bb7
David Andersen <davidx@gmail.com>
2025-04-07 19:55:39 -0400

    Fix: FIX_001, FIX_002 (2 issues)

--- test/test.py
+++ test/test.py
@@ -14,3 +14,11 @@ class TestDataEndpoint(unittest.TestCase):
 def test_invalid_endpoint(self):
     response = self.app.get('/invalid')
     self.assertEqual(response.status_code, 404)
+
+
+# Auto-fix for issue FIX_001:
+def test_data_endpoint(self):
+    DATA_ENDPOINT = '/data'
+    response = self.app.get(DATA_ENDPOINT)
+    self.assertEqual(response.status_code, 200)
+    self.assertListEqual(response.get_json(), [1, 2, 3, 4, 5])
