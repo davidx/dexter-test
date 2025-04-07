a7b21f3236ffef1db7fb3053ff2675dd53b28297
David Andersen <davidx@gmail.com>
2025-04-07 15:36:14 -0400

    Fix: BUG-001, CODE-002, CODE-003 (3 issues)

--- test/test.py
+++ test/test.py
@@ -1,16 +1,19 @@
 class TestDataEndpoint(unittest.TestCase):
+    DATA_ENDPOINT = '/data'
+
     def setUp(self):
         self.app = app.test_client()
 
-    def test_data_endpoint(self):
+    def test_data_endpoint_constant(self):
         DATA_ENDPOINT = '/data'
 
-    def test_data_endpoint(self):
+    def test_data_endpoint_response(self):
         response = self.app.get(self.DATA_ENDPOINT)
         self.assertEqual(response.status_code, 200)
         self.assertListEqual(response.get_json(), [1, 2, 3, 4, 5])
 
 
-def test_invalid_endpoint(self):
-    response = self.app.get('/invalid')
-    self.assertEqual(response.status_code, 404)
+def test_invalid_endpoint():
+    app = TestDataEndpoint()
+    response = app.app.get('/invalid')
+    app.assertEqual(response.status_code, 404)
