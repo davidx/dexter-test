fa25ef784febd8a6a597d0bf6414bd42fc234a31
David Andersen <davidx@gmail.com>
2025-04-07 21:52:53 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,16 +1,1 @@
-class TestDataEndpoint(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
 
-    def test_data_endpoint(self):
-        DATA_ENDPOINT = '/data'
-
-    def test_data_endpoint(self):
-        response = self.app.get(self.DATA_ENDPOINT)
-        self.assertEqual(response.status_code, 200)
-        self.assertListEqual(response.get_json(), [1, 2, 3, 4, 5])
-
-
-def test_invalid_endpoint(self):
-    response = self.app.get('/invalid')
-    self.assertEqual(response.status_code, 404)
